a653ff6f92eb96470005587c9b51c19e78f27820
juwitaw <sakura181094@yahoo.co.uk>
2019-02-14 06:19:15 +0700

    Solved: Test Mapping 5

--- test/Test_Mappings_5.sol
+++ test/Test_Mappings_5.sol
@@ -17,9 +17,9 @@ contract Test_Mappings_5 is Koans {
         alphabets[0] = "a";
         alphabets[1] = "b";
         alphabets[2] = "c";
-        Assert.equal(alphabets[0], __, "should return the 1st letter of alphabet");
-        Assert.equal(alphabets[1], __, "should return the 2nd letter of alphabet");
-        Assert.equal(alphabets[2], __, "should return the 3rd letter of alphabet");
+        Assert.equal(alphabets[0], "a", "should return the 1st letter of alphabet");
+        Assert.equal(alphabets[1], "b", "should return the 2nd letter of alphabet");
+        Assert.equal(alphabets[2], "c", "should return the 3rd letter of alphabet");
     }
 
     // Keys can be all basic data types, even "bytes" and "string"
@@ -30,8 +30,8 @@ contract Test_Mappings_5 is Koans {
     function test_map_keys_can_be_elementary_datatypes() public {
         bool_map[true] = true;
         string_map[str] = str;
-        Assert.isTrue(__, "should return valid value for bool key");
-        Assert.equal(string_map[str], __, "should return valid value for string key");
+        Assert.isTrue(bool_map[true], "should return valid value for bool key");
+        Assert.equal(string_map[str], str, "should return valid value for string key");
     }
 
     // Values can be complex data types, including arrays, structs, and mappings
@@ -39,19 +39,19 @@ contract Test_Mappings_5 is Koans {
     mapping(uint => uint[]) map_of_ints_array;
     mapping(uint => mapping(uint=>uint)) map_of_map_of_ints; 
     function test_map_values_can_be_complex_datatypes() public {
-        map_of_ints[0] = 1 ;
+        map_of_ints[0] = 1;
         map_of_ints_array[0] = [2, 3, 4];
         map_of_map_of_ints[0][0] = 5;
-        Assert.equal(map_of_ints[0], __, "should equal the correct value");
-        Assert.equal(map_of_ints_array[0][1], __, "should equal the correct array value");
-        Assert.equal(map_of_map_of_ints[0][0], __, "should equal the correct map value");
+        Assert.equal(map_of_ints[0], uint(1), "should equal the correct value");
+        Assert.equal(map_of_ints_array[0][1], uint(3), "should equal the correct array value");
+        Assert.equal(map_of_map_of_ints[0][0], uint(5), "should equal the correct map value");
     }
 
     // You can look up nonexistent keys and still get returned values
     // See Assert.sol for available assertion methods
     function test_all_keys_return_values() public {
-        Assert.__(alphabets[25], "should return default nil value");
-        Assert.__(alphabets[9999999], "should return default nil value");
+        Assert.isZero(alphabets[25], "should return default nil value");
+        Assert.isZero(alphabets[9999999], "should return default nil value");
     }
 
     // Based on the 4 reasons above, you cannot iterate over mappings in Solidity
@@ -60,13 +60,16 @@ contract Test_Mappings_5 is Koans {
     mapping(uint => uint) doubles;
     function test_save_keys_when_using_mappings() public {
         doubles[1] = 2;
+        keys_in_map.push(1);
         doubles[2] = 4;
+        keys_in_map.push(2);
         doubles[128] = 256;
+        keys_in_map.push(128);
         // TODO: add the keys to your keys array
         // __ 
         Assert.equal(keys_in_map.length, uint(3), "map should have 4 key value pairs");
         Assert.equal(keys_in_map[2], uint(128), "array should have the key 256");
-        Assert.equal(doubles[keys_in_map[1]], __, "should return the correct value for the key");
+        Assert.equal(doubles[keys_in_map[1]], uint(4), "should return the correct value for the key");
     }
 
     /* 
